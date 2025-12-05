45318279e97e6a455a3d9cb90331fcbe70e90440
Elsa Girardo <egirardo@hotmail.com>
2025-12-05 11:28:43 +0100

    Feature: Added script functionality. Page is now randomly chosen to product a random commander with every reload.

--- index.php
+++ index.php
@@ -10,28 +10,29 @@ require __DIR__ . '/header.php';
         <p>Select your desired color(s) below (or go crazy and switch on any color!) and click generate!</p>
     </section>
 
-    <div class="card-container"></div>
+    <div class="commander-container">
+        <div class="card-container"></div>
+        <div class="card-info"></div>
+    </div>
 
-    <!-- Create user input form to search API based on search criteria from user: Color, Set, Name, format legality, (maybe text content, depending on how complicated) -->
-
-    <form class="form-group" action="" method="post">
-        <label class="form-label">Color(s)</label>
+    <form class="form-group">
+        <label class="form-label">Desired Color(s)</label>
         <div class="btn-group">
-            <input class="btn-check" type="radio" id="white" value="White" />
-            <label class="btn btn-outline-primary" for="white">White</label>
-            <input class="btn-check" type="radio" id="blue" value="Blue" />
-            <label class="btn btn-outline-primary" for="blue">Blue</label>
-            <input class="btn-check" type="radio" id="black" value="White" />
-            <label class="btn btn-outline-primary" for="black">Black</label>
-            <input class="btn-check" type="radio" id="red" value="White" />
-            <label class="btn btn-outline-primary" for="red">Red</label>
-            <input class="btn-check" type="radio" id="green" value="White" />
-            <label class="btn btn-outline-primary" for="green">Green</label>
+            <input class="btn-check" type="checkbox" id="white" value="W" name="checkbox" />
+            <label class="btn btn-primary" for="white">White</label>
+            <input class="btn-check" type="checkbox" id="blue" value="U" name="checkbox" />
+            <label class="btn btn-primary" for="blue">Blue</label>
+            <input class="btn-check" type="checkbox" id="black" value="B" name="checkbox" />
+            <label class="btn btn-primary" for="black">Black</label>
+            <input class="btn-check" type="checkbox" id="red" value="R" name="checkbox" />
+            <label class="btn btn-primary" for="red">Red</label>
+            <input class="btn-check" type="checkbox" id="green" value="G" name="checkbox" />
+            <label class="btn btn-primary" for="green">Green</label>
         </div>
-        <div class="form-check form-switch">
-            <input class="form-check-input" type="checkbox" id="formSwitchChecked" checked>
+        <!-- <div class="form-check form-switch">
+            <input class="form-check-input" type="checkbox" id="formSwitchChecked">
             <label class="form-check-label" for="formSwitchChecked">Any color!</label>
-        </div>
+        </div> -->
         <button type="submit" class="btn btn-primary">Generate!</button>
     </form>
 
--- script.js
+++ script.js
@@ -1,24 +1,70 @@
-const url = `https://api.magicthegathering.io/v1/cards`;
+const maxPage = 100;
+const randomPage = Math.floor(Math.random() * maxPage);
 
 
+const formGroup = document.querySelector('.form-group');
+let colorSelection = "W"
+
+formGroup.addEventListener("submit", function(event){
+        event.preventDefault();
+        const formData = new FormData(event.target);
+        const allCheckboxValues = formData.getAll('checkbox');
+        console.log(allCheckboxValues); 
+        let colorSelection = allCheckboxValues.join(",");
+        console.log(colorSelection);
+})
+
+
+
+// how to set strict types in url? how to set multiple search parameters ie coloridentity and pages?
+const url = `https://api.magicthegathering.io/v1/cards?colorIdentity=${colorSelection}&page=${randomPage}`;
+
+
+// how to remove repeat results
 const getInfo = async () => {
     try {
         const response = await fetch(url);
 
         const info = await response.json();
 
-        const cardNumber = Math.random();
         console.log(info.cards);
-        console.log(info.cards[48].supertypes[0]);
+        
+        const onlyImages = info.cards.filter((card) => {
+            return card.imageUrl
+        })
+        //fix that below?
+        const strictColors = onlyImages.filter((card) => {
+            return card.colorIdentity===colorSelection;
+        })
+        const filtered = onlyImages.filter((card) => {
+            
+            return card.type?.includes('Legendary Creature')
+        })
+        
+        const max = (filtered.length); // The maximum value (exclusive)
+        const randomNumber = Math.floor(Math.random() * max);
+        
+        console.log(filtered[randomNumber]);
 
+        console.log('f: ', filtered)
+
+        const randomCard = filtered[randomNumber];
+
+
+        // add image
         const img = document.createElement('img');
         img.classList.add("card-img");
-        img.src = info.cards[48].imageUrl;
+        img.src = randomCard.imageUrl;
         img.height = 100;
         const cardImage = document.querySelector('.card-container');
-        console.log(cardImage);
         cardImage.appendChild(img);
 
+        //add name
+        const name = document.createElement('h2');
+        name.classList.add("card-name");
+        name.textContent = randomCard.name;
+        const cardName = document.querySelector('.card-info');
+        cardName.appendChild(name)
         
         
     } catch (error) {
